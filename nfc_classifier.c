c44bcf6249d90af5ee3593a6a9631052384836eb
Eloi Viciana <eloi.viciana@estudiants.urv.cat>
2026-09-26 15:41:10 +0200

    feat: estat de la targeta en un struct, la pantalla el llegeix. Y seguimos con el classifier

--- nfc_classifier.c
+++ nfc_classifier.c
@@ -1,16 +1,55 @@
 #include <furi.h>
 #include <gui/gui.h>
 
+//los tipos de targeta
+typedef enum {
+    CardTypeUnknown,     // per defecte, encara no classificada. La ponemos primero pq al no asignarle valor el campo numerico 
+                         //se queda a 0, por lo tanto una sin clasifucar y una desconocida son equivalentes
+    CardTypeUltralight,  // Ultralight / NTAG
+    CardTypeClassic1k,
+    CardTypeClassic4k,
+    CardTypeDesfire,
+} CardType;
+
+//ifnormacio que tenim del xip actual
+// struct con un solo campo de momento no me juzguen
+typedef struct {
+    CardType type;
+} CardInfo;
+
+
 // etiqueta para identificar los logs
 #define TAG "NfcClassifier"  
 
 
 // la funcion que la GUI llama para escribir en la pantalla
 static void nfc_classifier_draw_callback(Canvas* canvas, void* context) {
-    UNUSED(context);
+    CardInfo* info = context; //recuperem les dades
     canvas_clear(canvas); //borrem tot el q hi havia
     canvas_set_font(canvas, FontPrimary); 
-    canvas_draw_str(canvas, 2, 12, "NFC Classifier"); //suposo que x=2 i y=12??
+
+    switch (info->type)
+    {
+    case CardTypeUltralight:
+        canvas_draw_str(canvas, 4, 12, "Ultralight / NTAG");
+        break;
+    case CardTypeClassic1k:
+        canvas_draw_str(canvas, 4, 12, "Classic1k");
+        break;
+    case CardTypeClassic4k:
+        canvas_draw_str(canvas, 4, 12, "Classic4k");
+        break;
+    case CardTypeDesfire:
+        canvas_draw_str(canvas, 4, 12, "Desfire");
+        break;
+    
+    default:
+        canvas_draw_str(canvas, 4, 12, "Desconeguda");
+        break;
+    }
+
+
+    canvas_draw_str(canvas, 50, 50, "NFC Classifier"); //suposo que x=2 i y=12??
 }
 
 // cada vez que se pulse un boton la GUI llama a esta funcion
@@ -26,10 +65,14 @@ static void nfc_classifier_input_callback(InputEvent* event, void* context) {
 
 // funcion de entrada
 int32_t nfc_classifier_app(void* p) {
-    UNUSED(p);                 // para silenciar el warning?
+    UNUSED(p);                 // para silenciar el warning 
+
+    // de moment fixem el tipus a mà, per provar sense NFC
+    CardInfo card = { .type = CardTypeClassic1k };  
 
     //bustia per rebre els esdeveniments de botons
-    //pongo un 8 porque no es fisicamente posible pulsar mas de 8 botones a la vez (?)
+    //pongo un 8 porque me apetece, un limite comodo de eventos acumulados en la cola,
+    //es imposible que se llene, a menos que venga una mega rafaga
     FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
     
     //pido prestado el servicio de la pantalla
@@ -39,7 +82,7 @@ int32_t nfc_classifier_app(void* p) {
     ViewPort* view_port= view_port_alloc();
 
     // i li donem la funcio de dibuixar
-    view_port_draw_callback_set(view_port, nfc_classifier_draw_callback, NULL); 
+    view_port_draw_callback_set(view_port, nfc_classifier_draw_callback, &card); 
 
     // registramos el callback de input y se lo damos a la cola como context
     view_port_input_callback_set(view_port, nfc_classifier_input_callback, event_queue);
@@ -73,7 +116,7 @@ int32_t nfc_classifier_app(void* p) {
     furi_message_queue_free(event_queue); 
 
 
-    FURI_LOG_I(TAG, "missatge");    // deixa constància que hem entrat
+    FURI_LOG_I(TAG, "app tancada");    // deixa constància que hem entrat
     return 0;                  // sortida neta
 }
 
